2cbc423c0aadd9bc76178d7c92900552209e5e6d
Rodrigo <rodrigo.moni@inf.ufrgs.br>
2019-05-03 22:39:52 -0300

    Added d3v4 and finished the slider similarity behavior

--- javascripts/similarity-force-chart.js
+++ javascripts/similarity-force-chart.js
@@ -36,7 +36,18 @@ function similarityForce()
             .on("end", dragended);
     }
 
-    function update(data) {
+    function update(data, hmtlContent) {
+
+        svg = d3v4.select(hmtlContent)
+            .classed("svg-container", true) //container class to make it responsive
+            .append("svg")
+            .attr("preserveAspectRatio", "xMinYMin meet")
+            .attr("viewBox", [-outerWidth / 2, -outerHeight / 2 + 50, outerWidth, outerHeight])
+            //class to make it responsive
+            .classed("svg-content-responsive", true)
+            .classed("similarity-force", true)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
         const links = data.links.map(function (d){ return Object.create(d)});
         const nodes = data.nodes.map(function(d){return Object.create(d)});
@@ -86,38 +97,38 @@ function similarityForce()
     }
 
     function filterEdges(data, similarity) {
-        data.links =  data.links.filter(function (d) {
+        var result = {};
+
+        result.links =  data.links.filter(function (d) {
             return d.value >= similarity;
         });
 
-        return data;
+        result.nodes = data.nodes;
+
+        return result;
     }
 
     function chart(selection) {
         selection.each(function (data) {
             panelID = ($(this).parents('.panel')).attr('id');
+            var panel = this;
 
             $("#"+ panelID + " .panel-body")
-                .append('Select the value of %: <input id= "slider-similarity-' + panelID+  '" type="text" data-slider-min="55" data-slider-max="100" data-slider-step="5" data-slider-value="75"/>');
+                .append('Select the value of %: <input id= "slider-similarity-' + panelID+  '" type="text" data-slider-min="70" data-slider-max="100" data-slider-step="1" data-slider-value="85"/>');
 
             var mySlider = $("#slider-similarity-" + panelID).bootstrapSlider({
                     tooltip_position: 'bottom'
                 });
 
+            mySlider.on("slideStop", function(slideEvt) {
+                var similarity = slideEvt.value;
+                $(panel).find('svg').remove();
+                var newData = filterEdges(data, similarity);
+                update(newData, panel);
+            });
 
-            svg = d3v4.select(this)
-                .classed("svg-container", true) //container class to make it responsive
-                .append("svg")
-                .attr("preserveAspectRatio", "xMinYMin meet")
-                .attr("viewBox", [-outerWidth / 2, -outerHeight / 2 + 50, outerWidth, outerHeight])
-                //class to make it responsive
-                .classed("svg-content-responsive", true)
-                .classed("similarity-force", true)
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-            update(filterEdges(data, 75));
-            //update();
+            var newData =  filterEdges(data, 85);
+            update(newData, this);
         })
     }
 
