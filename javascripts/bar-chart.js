0cd7f3fc4ef152eb8721dfab0a01f8986f932ec3
Rodrigo Moni <rodrimoni@gmail.com>
2024-10-04 22:29:46 -0300

    feat: sort bars

--- javascripts/bar-chart.js
+++ javascripts/bar-chart.js
@@ -5,128 +5,150 @@ function barChart(typeChart) {
     var width = MAX_WIDTH;
     var height = MAX_HEIGHT;
 
-    const numberOfCategories = 10
-
     var div = d3.select(".toolTip");
+
+    let panelID = "";
 
     const getCategoryColor = d => {
         if (typeChart === THEMES_BAR_CHART) {
-            return CONGRESS_DEFINE.subjectsToColor[d]
+            return CONGRESS_DEFINE.subjectsToColor[d];
         }
-
         return CONGRESS_DEFINE.getPartyColor(d);
-    }
+    };
 
     const getCategoryLabel = d => {
         if (typeChart === THEMES_BAR_CHART) {
-            return language === ENGLISH ? subjectsToEnglish[d] : d
+            return language === ENGLISH ? subjectsToEnglish[d] : d;
         }
-
         return d;
-    }
-
-    const getChartTitle = () => {
-        return typeChart === THEMES_BAR_CHART ? 'Most Discussed Subjects' : 'Largest Political Parties';
-    }
+    };
 
     function chart(selection) {
         selection.each(function (data) {
-            data = data.slice(0, numberOfCategories); // pegar somente um determinado numero de categorias
+            // Add the checkbox for sorting
+            var checkboxContainer = d3.select(this)
+                .append("div")
+                .attr("class", "checkbox-container")
+                .attr("style", "margin-top:20px; margin-left: 20px; position: absolute");
 
-            var max = d3.max(data, function (d) { return d.frequency });
-            var labelWidth = 0;
-            var valueMargin = 2;
+            checkboxContainer.append("label")
+                .text("Sort Alphabetically")
+                .append("input")
+                .attr("type", "checkbox")
+                .attr("class", "sortCheckbox");
 
-            var barHeight = (height - axisMargin - marginY * 2) * 0.4 / data.length,
-                barPadding = (height - axisMargin - marginY * 2) * 0.6 / data.length;
+            // Store original data for sorting reference
+            var originalData = data.slice();
 
-            var svg = d3.select(this)
-                .append("svg")
-                .attr("width", "100%")
-                .attr("height", "100%")
-                .attr("viewBox", "0 0 " + width + " " + height)
-                .classed("bar-chart", true);
+            panelID = ($(this).parents('.panel')).attr('id');
 
-            // Add title to the chart
-            svg.append("text")
-                .attr("x", width / 2)   // Center the title
-                .attr("y", -100)  // Position slightly below the top margin
-                .attr("text-anchor", "middle")
-                .style("font-size", "35px")  // Adjust the font size as needed
-                .style("font-weight", "bold")
-                .text(getChartTitle);  // Set the chart title
+            function render(sortedData) {
+                // Clear previous chart if it exists
+                d3.select("#" + panelID + " .bar-chart").remove();
 
-            var bar = svg.selectAll("g")
-                .data(data)
-                .enter()
-                .append("g");
+                var max = d3.max(sortedData, function (d) { return d.frequency });
+                var labelWidth = 0;
+                var valueMargin = 2;
 
-            bar.attr("class", "bar")
-                .attr("cx", 0)
-                .attr("fill", function (d) { return getCategoryColor(d.category); })
-                .attr("transform", function (d, i) {
-                    return "translate(" + marginX + "," + (i * (barHeight + barPadding) + barPadding) + ")";
-                });
+                var barHeight = (height - axisMargin - marginY * 2) * 0.6 / sortedData.length;
+                var barPadding = (height - axisMargin - marginY * 2) * 0.4 / sortedData.length;
 
-            bar.append("text")
-                .attr("class", "label")
-                .attr("y", barHeight / 2)
-                .attr("dy", ".35em") //vertical align middle
-                .text(function (d) {
-                    return getCategoryLabel(d.category);
-                }).each(function () {
-                    labelWidth = Math.ceil(Math.max(labelWidth, this.getBBox().width));
-                })
-                .style("pointer-events", "none");  // Disable pointer events for text;
+                var svg = d3.select(selection.node())
+                    .append("svg")
+                    .attr("width", "100%")
+                    .attr("height", "100%")
+                    .attr("viewBox", "0 0 " + width + " " + height)
+                    .classed("bar-chart", true);
 
-            var scale = d3.scale.linear()
-                .domain([0, max])
-                .range([0, width - marginX * 2 - labelWidth]);
+                var bar = svg.selectAll("g")
+                    .data(sortedData)
+                    .enter()
+                    .append("g");
 
-            var xAxis = d3.svg.axis()
-                .scale(scale)
-                .tickSize(-height + 2 * marginY)
-                .orient("bottom");
+                bar.attr("class", "bar")
+                    .attr("fill", function (d) { return getCategoryColor(d.category); })
+                    .attr("transform", function (d, i) {
+                        return "translate(" + marginX + "," + (i * (barHeight + barPadding) + barPadding) + ")";
+                    });
 
-            bar.append("rect")
-                .attr("transform", "translate(" + labelWidth + ", 0)")
-                .attr("height", barHeight)
-                .attr("width", function (d) {
-                    return scale(d.frequency);
-                });
+                bar.append("text")
+                    .attr("class", "label")
+                    .attr("y", barHeight / 2)
+                    .attr("dy", ".35em")
+                    .text(function (d) {
+                        return getCategoryLabel(d.category);
+                    }).each(function () {
+                        labelWidth = Math.ceil(Math.max(labelWidth, this.getBBox().width));
+                    })
+                    .style("pointer-events", "none");
 
-            bar.append("text")
-                .attr("class", "value")
-                .attr("y", barHeight / 2)
-                .attr("dx", labelWidth + 10) //margin right
-                .attr("dy", ".35em") //vertical align middle
-                .attr("text-anchor", "start")
-                .text(function (d) {
-                    return d.frequency
-                })
-                .attr("x", function (d) {
-                    var width = this.getBBox().width;
-                    return Math.max(width + valueMargin, scale(d.frequency));
-                })
-                .style("pointer-events", "none");  // Disable pointer events for text;
+                var scale = d3.scale.linear()
+                    .domain([0, max])
+                    .range([0, width - marginX * 2 - labelWidth]);
 
-            bar
-                .on("mousemove", function (d) {
+                var xAxis = d3.svg.axis()
+                    .scale(scale)
+                    .tickSize(-height + 2 * marginY)
+                    .orient("bottom");
+
+                bar.append("rect")
+                    .attr("transform", "translate(" + labelWidth + ", 0)")
+                    .attr("height", barHeight)
+                    .attr("width", function (d) {
+                        return scale(d.frequency);
+                    });
+
+                bar.append("text")
+                    .attr("class", "value")
+                    .attr("y", barHeight / 2)
+                    .attr("dx", labelWidth + 10)
+                    .attr("dy", ".35em")
+                    .attr("text-anchor", "start")
+                    .text(function (d) {
+                        return d.frequency;
+                    })
+                    .attr("x", function (d) {
+                        var width = this.getBBox().width;
+                        return Math.max(width + valueMargin, scale(d.frequency));
+                    })
+                    .style("pointer-events", "none");
+
+                bar.on("mousemove", function (d) {
                     div.style("left", d3.event.pageX + 10 + "px");
                     div.style("top", d3.event.pageY - 25 + "px");
                     div.style("display", "inline-block");
                     div.html(getCategoryLabel(d.category) + "<br>" + d.frequency);
                 });
-            bar
-                .on("mouseout", function () {
+
+                bar.on("mouseout", function () {
                     div.style("display", "none");
                 });
 
-            svg.insert("g", ":first-child")
-                .attr("class", "axisHorizontal")
-                .attr("transform", "translate(" + (marginX + labelWidth) + "," + (height - axisMargin - marginY) + ")")
-                .call(xAxis);
+                svg.insert("g", ":first-child")
+                    .attr("class", "axisHorizontal")
+                    .attr("transform", "translate(" + (marginX + labelWidth) + "," + (height - axisMargin - marginY) + ")")
+                    .call(xAxis);
+            }
 
+            // Initial render with unsorted data
+            render(data);
+
+            // Handle checkbox change event for sorting
+            d3.select("#" + panelID + " .sortCheckbox").on("change", function () {
+                if (this.checked) {
+                    // Sort data alphabetically
+                    var sortedData = data.slice().sort(function (a, b) {
+                        if (language === ENGLISH) {
+                            return d3.ascending(subjectsToEnglish[a.category], subjectsToEnglish[b.category]);
+                        }
+                        return d3.ascending(a.category, b.category);
+                    });
+                    render(sortedData);
+                } else {
+                    // Use the original data order
+                    render(originalData);
+                }
+            });
         });
     }
 
